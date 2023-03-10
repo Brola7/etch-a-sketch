15b00bba41df1184c96363a3a9d2f9ec3e193431
Rati Broladze <ratibroladze2004@gmail.com>
2023-03-10 20:01:11 +0400

    Creat drawing grid

---
+++ script.js
@@ -0,0 +1,28 @@
+function createNew(size){
+    const cont = document.createElement("div");
+    const container = document.querySelector(".container");
+    for(let i = 0; i < size; i++){
+        let pixelRow = document.createElement("div");
+        pixelRow.classList.toggle("pixelRow");
+        for(let i = 0; i < size; i++){
+            let pixel = document.createElement("div");
+            pixel.classList.toggle("eachPixel");
+            
+            pixelRow.appendChild(pixel);
+        }
+        cont.appendChild(pixelRow);
+    }
+    cont.classList.toggle("cont");
+    container.appendChild(cont);
+}
+
+createNew(16);
+const allPixels = document.querySelectorAll(".eachPixel");
+allPixels.forEach(eachPixel => {
+    eachPixel.addEventListener("mouseover", () => {
+        eachPixel.classList.remove("eachPixel");
+        eachPixel.classList.add("eachPixelEvent");
+    })
+});
+
+
